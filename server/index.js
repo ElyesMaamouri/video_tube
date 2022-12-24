5b459eb9f7c3867f93c88ea60c8d6e5c3348d859
ElyesMaamouri <88774647+ElyesMaamouri@users.noreply.github.com>
2022-12-24 09:45:50 +0100

    Setup api in font-end project & add videos in page Home

--- server/index.js
+++ server/index.js
@@ -6,10 +6,10 @@ import videoRoutes from "./routes/videos.js"
 import commentRoutes from "./routes/comments.js"
 import authRoutes from "./routes/auth.js"
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 //Create server
 const app = express();
-
+app.use(cors())
 dotenv.config();
 
 //Connect to database
